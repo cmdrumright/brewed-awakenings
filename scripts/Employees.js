e420a6daf68b632f95893936531b64b4b6bd4082
cmdrumright <cdrumright@protonmail.com>
2025-12-01 14:23:04 -0600

    added employee state data and click event listener to display product sold count

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,12 +1,20 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
-const employees = getEmployees()
+addEventListener("click", (clickEvent) => {
+    const clickedItem = clickEvent.target
+    if (clickedItem.dataset.type === "employee") {
+        const allOrders = getOrders()
+        const orderCount = allOrders.filter((order) => order.employeeId === parseInt(clickedItem.dataset.id)).length
+        window.alert(`${clickedItem.dataset.name} sold ${orderCount} products`)
+    }
+})
 
 export const Employees = () => {
+    const employees = getEmployees()
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li>${employee.name}</li>`
+        html += `<li data-type="employee" data-id="${employee.id}" data-name="${employee.name}">${employee.name}</li>`
     }
 
     html += "</ul>"
