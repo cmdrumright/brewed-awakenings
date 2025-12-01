95bd93caa761431aeae779cb9b88e0002107503f
cmdrumright <cdrumright@protonmail.com>
2025-12-01 13:53:22 -0600

    added state data and click event handler to read and display they data

--- scripts/Products.js
+++ scripts/Products.js
@@ -1,12 +1,24 @@
 import { getProducts } from "./database.js"
 
-const products = getProducts()
+addEventListener("click", (clickEvent) => {
+    const clickedItem = clickEvent.target;
+    if (clickedItem.dataset.type === "product") {
+        window.alert(`${clickedItem.dataset.name}\n$${clickedItem.dataset.price}`)
+    }
+})
 
 export const Products = () => {
+
+    const products = getProducts()
+
     let html = "<ul>"
 
     for (const product of products) {
-        html += `<li>${product.name}</li>`
+        html += `<li data-type="product"
+            data-price="${product.price}"
+            data-name="${product.name}">
+                ${product.name}
+            </li>`
     }
 
     html += "</ul>"
